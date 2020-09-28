f90312e06a194a109d9b89b0a2f2db5caa01c166
NishithaReddy0909 <nishithareddynalla0909@gmail.com>
2020-09-28 11:18:19 +0530

    Creted Edit Action to ProductsController

--- BakeryProject/Controllers/ProductsController.cs
+++ BakeryProject/Controllers/ProductsController.cs
@@ -16,5 +16,9 @@ namespace BakeryProject.Controllers
         {
             return View();
         }
+        public IActionResult Edit()
+        {
+            return View();
+        } 
     }
 }
